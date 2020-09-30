85fd7dc10c138f8276f9c66dd66bd7d72b775f89
Luiza Cintra Fernandes <luiza.fernandes@arcadis.com>
2020-09-30 09:36:58 -0300

    login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,30 +3,43 @@ const fs = require('fs');
 const path = require('path');
 
 const UserController = {
-    cadastro: (req,res)=>{
+    cadastro: (req, res) => {
         res.render('menu-user')
     },
-    cadastrar: (req,res) =>{
-        console.log(req.body)
-        let {nome,email,senha}=req.body;
+    cadastrar: (req, res) => {
+        //console.log(req.body)
+        let { nome, email, senha } = req.body;
         //let img = '/img/users/'+req.files[0].originalname;
-        console.log(req.files)
         let id = listaUsers.length + 1
         listaUsers.push({
-            id:id,
-            nome:nome,
-            email:email,
-            senha:senha,
-           // img:img
+            id: id,
+            nome: nome,
+            email: email,
+            senha: senha,
+            // img:img
         })
 
         fs.writeFileSync(path.join('database', 'User.json'), JSON.stringify(listaUsers));
-        
+
         res.redirect('/user/cadastro');
 
+    },
+    show: (req, res) => {
+        res.render('login')
+    },
+    login: (req, res) => {
+        console.log(req.body)
+        let { email, senha } = req.body;
+        let findUser = listaUsers.find(lista => lista.email == email && lista.senha == senha)
+        console.log(findUser)
+        if (findUser == undefined) {
+            res.render('login')
+        } else {
+            res.render('menu-user')
+        }
 
     }
 
 }
 
-module.exports=UserController
+module.exports = UserController
--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var Router = express.Router();
-const multer = require('multer');
+// const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null, 'public/img/users')
-    },
-    filename:(req,file, cb) => {
-        cb(null, file.originalname)
-    }
-}); 
-const upload = multer({ storage })
+// const storage = multer.diskStorage({
+//     destination: (req,file,cb) => {
+//         cb(null, 'public/img/users')
+//     },
+//     filename:(req,file, cb) => {
+//         cb(null, file.originalname)
+//     }
+// }); 
+// const upload = multer({ storage })
 
 const UserController = require('../controllers/UserController');
 
-Router.get('/cadastro',UserController.cadastro)
-Router.post('/cadastro',upload.any(),UserController.cadastrar)
-//Router.get('/login',UserController.login)
+Router.get('/cadastro', UserController.cadastro)
+Router.post('/cadastro', UserController.cadastrar)
+Router.get('/login', UserController.show)
+Router.post('/login', UserController.login)
 
-module.exports=Router
+module.exports = Router
